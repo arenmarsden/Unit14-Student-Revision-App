9055c17fccfbdfb6f85cff5ed4f17a084f9e9c54
Aren Marsden <aren.marsden6@gmail.com>
2022-01-22 23:43:01 +0000

    Initial Commit
    Signed-off-by: Aren Marsden <aren.marsden6@gmail.com>

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace StudentRevisionApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(671, 629);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(200, 61);
+            this.checkButton.TabIndex = 0;
+            this.checkButton.Text = "Check";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 716);
+            this.Controls.Add(this.checkButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button checkButton;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace StudentRevisionApp
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<Label, TextBox> _keyValuePairs = new();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] questions =
+            {
+                "9 * 10",
+                "10 * 12",
+                "8 * 4",
+                "2 * 12",
+                "(0.25 * 100) / 10",
+                "0.072 * 32",
+            };
+
+            var random = new Random();
+            var questionNum = 0;
+
+            const int pointX = 30;
+            const int pointY = 40;
+
+            var prevPointY = 0;
+
+            do
+            {
+                Label label = new();
+                label.Text = questions[random.Next(questions.Length)];
+                label.Location = new Point(pointX, prevPointY == 0 ? pointY : prevPointY + 40);
+                label.AutoSize = true;
+                label.BackColor = Color.LightGray;
+                label.Font = new Font("Arial", 14);
+
+                this.Controls.Add(label);
+
+                TextBox textBox = new TextBox();
+                textBox.Location = new Point(label.Location.X + 200, label.Location.Y);
+                textBox.BackColor = Color.LightGray;
+                
+                Controls.Add(textBox);
+                _keyValuePairs[label] = textBox;
+                prevPointY = pointY;
+                questionNum++;
+            } while (questionNum != 5);
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            foreach (var pair in _keyValuePairs)
+            {
+                var label = pair.Key;
+                string[] numberArray = label.Text.Split('*');
+                
+                
+
+            }
+        }
+    }
+}
